986c3c46b377e32fed09a218741e37e77d1ddb79
Yuliia Drahan <dragan1925@gmail.com>
2020-10-07 16:47:30 +0300

    Shell Sort

---
+++ src/com/week/one/ShellSort.java
@@ -0,0 +1,23 @@
+package com.week.one;
+
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] array = {30, 2, 10, 4, 6};
+        int length = array.length;
+        int i,j;
+        for (i=0; i<length; i++){
+            for (j=length-1; j>i; j--) {
+                if (array[j]<array[j-1]){
+                    int temp = array[j];
+                    array[j]=array[j-1];
+                    array[j-1]=temp;
+                }
+            }
+
+        }
+        for (i=0; i<length; i++){
+            System.out.print(array[i]+" ");
+        }
+    }
+
+}
